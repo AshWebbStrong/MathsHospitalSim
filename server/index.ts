25b7ec8deb554db3ade7065f5ee56ac0f8591360
Ash Webb-Strong <ash-webby@hotmail.com>
2025-07-06 15:20:38 +0100

    add root route for health check

--- server/index.ts
+++ server/index.ts
@@ -3,13 +3,17 @@ import { Server } from 'colyseus';
 import http from 'http';
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from the Hospital Math Sim backend!');
+});
+
 const server = http.createServer(app);
 const gameServer = new Server({
   server,
 });
 
-const port = 2567;
-
+const port = process.env.PORT || 2567;
 server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
